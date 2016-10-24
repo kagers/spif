d7d7daeb3e6bd24072f8f510df4e58f86d854a0f
kagers <ka.gershfeld@gmail.com>
2016-10-24 17:53:05 -0400

    redo directories

---
+++ js/templates/rt_quasar_j15/scroll.js
@@ -0,0 +1,18 @@
+// JavaScript Document
+
+/* Random and scrolling background animation */
+
+ 
+var pos=0, speed=150;window.onload=function() {randomImage(); scrollIt(); }
+function randomImage() {
+var bgImageTotal=3;
+var randomNumber = Math.round(Math.random()*(bgImageTotal-1))+1;
+var imgPath='images/templates/rt_quasar_j15/backgrounds/'+randomNumber+'.jpg';
+document.getElementById('rt-top').style.backgroundImage="url("+imgPath+")";
+}
+function scrollIt() {
+
+document.getElementById('rt-top').style.backgroundPosition=pos+'px 0px';
+pos--;  // specifies left / right or top / bottom.
+setTimeout('scrollIt()',speed);
+}
